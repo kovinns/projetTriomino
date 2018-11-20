73da83460337a7669078eb85dd0e80772f49c15a
kovinns <kovinns@hotmail.fr>
2018-11-20 09:23:06 +0100

    avancé plateau

--- src/plateau/Plateau.java
+++ src/plateau/Plateau.java
@@ -2,5 +2,34 @@ package plateau;
 
 public class Plateau{
 
+  private ArrayList<ArrayList<PaireTriominos>>[] plateau;
+
+  public Plateau(){
+    this.plateau = new ArrayList<ArrayList<PaireTriominos>>[4];
+    ArrayList<ArrayList<PaireTriominos>> liste = new ArrayList<ArrayList<PaireTriominos>>();
+    this.plateau[0] = liste
+    this.plateau[1] = new ArrayList<ArrayList<PaireTriominos>>();
+    this.plateau[2] = new ArrayList<ArrayList<PaireTriominos>>();
+    this.plateau[3] = new ArrayList<ArrayList<PaireTriominos>>();
+    ArrayList<PaireTriominos> sousLite = new ArrayList<PaireTriominos>();
+    liste.add(sousListe);
+    sousListe.add(PaireTriominos);
+  }
+
+  public void ajoutTriomino(Triomino t, int x, int y){
+    if(x >= 0){
+      if(y >= 0){
+        
+      }else{
+
+      }
+    }else{
+      if(y >= 0){
+
+      }else{
+
+      }
+    }
+  }
 
 }
